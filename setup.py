44725bdfdf57617786a172b022f1581d4a75b78b
Jiho Lee <160725408+JihoTheCoder0607@users.noreply.github.com>
2024-04-17 20:28:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Jiho Lee',                   # Type in your name
   author_email = 'life120607@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/JihoTheCoder0607',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JihoTheCoder0607/NumPleX/archive/refs/tags/1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JihoTheCoder0607/CalPleX/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['Math', 'Graphs', 'Geometry'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
